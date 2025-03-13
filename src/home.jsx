88d3aee844018fdd95e67538a938b9996394ce05
emsaad10 <120191461+emsaad10@users.noreply.github.com>
2025-03-13 07:59:50 +0530

    saad heer

---
+++ src/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import { SignInButton } from '@clerk/clerk-react'
+
+
+function Home() {
+  return (
+    <div>
+      home 
+    </div>
+  )
+}
+
+export default Home
